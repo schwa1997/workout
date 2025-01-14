d48fbe0991c6803dee2e2773cee13bedf66d25ef
schwa1997 <huimin.chen@studenti.unipd.it>
2025-01-14 14:06:10 +0100

    update the content

--- src/app/data/blog.tsx
+++ src/app/data/blog.tsx
@@ -1,19 +1,19 @@
 import { blog } from "./type";
 
-
 export const blogs: blog[] = [
   {
     id: 1,
-    title: "瘦下来一定好看？",
-    tags: ["female", "knowledge"],
-    content: "不过，如果你非得让我抉择，我只能说：嗯，大多数人在这个垃圾食品横行、压力大到爆炸的世界里，瘦一点确实可能意味着饮食健康、生活方式积极。至少不会因为走个楼梯就差点“阵亡”。所以，用健康的方式保持体重，还是不错的选择。\n\n但请记住：健康和自信才是真正的“美”。无论你是“瘦如竹竿”还是“心宽体胖”，最重要的还是做真实的自己。",
-    img:"https://static1.keepcdn.com/2018/04/22/14/1524379155506_1080x1080.jpg",
+    title: "How To Fix Anterior Pelvic Tilt Posture ",
+    tags: ["brain-workout", "knowledge"],
+    content:
+      "不过，如果你非得让我抉择，我只能说：嗯，大多数人在这个垃圾食品横行、压力大到爆炸的世界里，瘦一点确实可能意味着饮食健康、生活方式积极。至少不会因为走个楼梯就差点“阵亡”。所以，用健康的方式保持体重，还是不错的选择。\n\n但请记住：健康和自信才是真正的“美”。无论你是“瘦如竹竿”还是“心宽体胖”，最重要的还是做真实的自己。",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.91183.net%2Fwp-content%2Fuploads%2F2019%2F07%2F5845664.jpg&f=1&nofb=1&ipt=f0d4a1b4836a60c158df73754e06e7d1d678f0c304851e105b1ba6659985b607&ipo=images",
   },
   {
     id: 2,
-    title: "title2",
-    tags: ["female", "knowledge"],
+    title: "workout Posture",
+    tags: ["workout", "brain-workout"],
     content: "first blog",
-    img:"https://static1.keepcdn.com/2018/04/22/14/1524379155506_1080x1080.jpg",
+    img: "https://wellnesszing.com/wp-content/uploads/2018/06/Exercise-for-Correct-Posture-WellnessZing.jpg",
   },
 ];
